366d15d48b07b47a96031f356809d3da5ded80e9
MinseonCho <seoon53@gmail.com>
2024-02-10 01:16:30 +0900

    feat: 빈 쿼리 아이템 생성하는 메서드 추가

--- composeApp/src/commonMain/kotlin/model/QueryItem.kt
+++ composeApp/src/commonMain/kotlin/model/QueryItem.kt
@@ -4,4 +4,12 @@ data class QueryItem(
     var key: String,
     var value: String,
     var isChecked: Boolean = true
-)
+) {
+    companion object {
+        fun generateEmptyQueryItem(): QueryItem = QueryItem(
+            key = "",
+            value = "",
+            isChecked = false
+        )
+    }
+}
